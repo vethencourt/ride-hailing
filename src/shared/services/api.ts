3cba24782bf388cae339a074193d6ff393c8b533
Ricardo Vethencourt <ricardo.vethencourt.m@gmail.com>
2026-01-21 21:56:31 -0400

    chore: setup axios

---
+++ src/shared/services/api.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+// import { useAuthStore } from '@/auth/store'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+// request interceptor adds JWT
+// api.interceptors.request.use((config) => {
+//   const authStore = useAuthStore()
+//   if (authStore.token) {
+//     config.headers.Authorization = `Bearer ${authStore.token}`
+//   }
+//   return config
+// })
+
+// response interceptor handles global errors
+api.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    const message = error.response?.data?.message || 'Something went wrong'
+    // TODO: maybe trigger a Quasar Notify
+    return Promise.reject(new Error(message))
+  }
+)
+
+export default api
