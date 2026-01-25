c066e236932ca547f598432c35022499f5a29d0e
Ricardo Vethencourt <ricardo.vethencourt.m@gmail.com>
2026-01-24 23:07:51 -0400

    refactor: create back button component

---
+++ src/vehicles/pages/BackButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
+<template>
+  <q-btn
+    class="back-button"
+    icon="keyboard_arrow_left"
+    text-color="dark-text"
+    @click="router.back()"
+    flat
+  />
+</template>
+
+<style lang="scss" scoped></style>
--- src/vehicles/pages/VehicleCreatePage.vue
+++ src/vehicles/pages/VehicleCreatePage.vue
@@ -1,25 +1,14 @@
-VehicleCreatePageVehicleCreatePage
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-
 import VehicleForm from '@/vehicles/components/VehicleForm.vue'
 
+import BackButton from './BackButton.vue'
 import { CREATE_TITLE } from '../constants'
-
-const router = useRouter()
 </script>
 
 <template>
   <q-page>
     <div class="header-container">
-      <q-btn
-        class="back-button"
-        icon="keyboard_arrow_left"
-        text-color="dark-text"
-        @click="router.back()"
-        round
-        flat
-      />
+      <back-button />
       <h5>{{ CREATE_TITLE }}</h5>
     </div>
     <VehicleForm />
@@ -31,7 +20,7 @@ const router = useRouter()
   padding: 0 1rem;
 
   .header-container {
-    .q-btn {
+    :deep(.back-button) {
       position: fixed;
     }
 
