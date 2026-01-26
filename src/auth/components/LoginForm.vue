1c55339c872c1dcbc44a249d263ebbae2d2f4eec
Ricardo Vethencourt <ricardo.vethencourt.m@gmail.com>
2026-01-26 08:58:23 -0400

    feat: change routes on login, implement logout, add app logo

--- src/auth/components/LoginForm.vue
+++ src/auth/components/LoginForm.vue
@@ -4,7 +4,7 @@ import { useRouter } from 'vue-router'
 
 import type { LoginCredentials } from '../types'
 
-import { AUTH_SIGNUP } from '@/shared/constants/routeNames'
+import { AUTH_SIGNUP, VEHICLE_LIST } from '@/shared/constants/routeNames'
 import { checkRequired, checkEmail } from '@/shared/utils/form'
 
 import { useAuthStore } from '../store'
@@ -15,10 +15,15 @@ const form = ref<LoginCredentials>({
   email: '',
   password: ''
 })
+
+function handleLogin() {
+  store.login(form.value)
+  router.push({ name: VEHICLE_LIST })
+}
 </script>
 
 <template>
-  <q-form class="column" @submit="store.login(form)">
+  <q-form class="column" @submit="handleLogin">
     <q-input
       placeholder="Correo"
       v-model="form.email"
--- src/shared/components/HeaderComponent.vue
+++ src/shared/components/HeaderComponent.vue
@@ -1,16 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+import { useAuthStore } from '@/auth/store'
+
+import { AUTH_LOGIN } from '../constants/routeNames'
+
+const router = useRouter()
+const store = useAuthStore()
+
+function handleLogout() {
+  store.logout()
+  router.push({ name: AUTH_LOGIN })
+}
+</script>
 
 <template>
   <q-header class="bg-primary" elevated>
     <q-toolbar>
       <q-toolbar-title>
         <q-avatar>
-          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          <img src="/logo.png" />
         </q-avatar>
         Ride Hailing
       </q-toolbar-title>
+      <q-btn icon="power_settings_new" @click="handleLogout" flat dense />
     </q-toolbar>
   </q-header>
 </template>
-
-<style scoped lang="scss"></style>
