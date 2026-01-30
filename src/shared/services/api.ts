5ac22d6d89e0c63f1cb16baca8e93aba22738946
Ricardo Vethencourt <ricardo.vethencourt.m@gmail.com>
2026-01-30 08:29:12 -0400

    fix: place correct env var in axios service

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useAuthStore } from '@/auth/store'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
